26d51b6139a734284f13f1de3348ede51f5ddf02
kanikasrinivasan <kanikasrinivasan4@gmail.com>
2025-11-19 21:58:40 +0530

    done

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://doctor-appointment-backend-wsfg.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://doctor-appointment-backend-1-8zvm.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
